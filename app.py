07c3d1b99f957e20595734e9e996f3fa3b5ac377
Dadi-Dinesh <dadidinesh4884@gmail.com>
2026-02-24 10:07:58 +0530

    feat: Update page icon to a custom image file.

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ import altair as alt
 
 st.set_page_config(
     page_title="CreditIQ — Credit Risk Intelligence",
-    page_icon="C",
+    page_icon="images/design-a-clean-minimal-black-and-white-l_tet7fcgyRKSPS-c0YaYOWQ_wgYqleLoQrOLRep7j-l0EA_sd.jpeg",
     layout="wide",
     initial_sidebar_state="auto"
 )
